d014dbabe03720fc276302ec5f8f6c1055f89aee
jeremy perez <glyde06@hotmail.com>
2017-09-04 21:21:36 -0500

    Added rack-cors config
    Now we can do typical crud http requests to this api from the Christian Hackers
    react app, as long as it's on localhost:3000 (for now).

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module ChristianHackersApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end    
   end
 end
