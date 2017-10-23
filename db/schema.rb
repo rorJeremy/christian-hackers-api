2098bcbdcc56fcb6bcacff7c33823265e6c3a675
jeremy perez <glyde06@hotmail.com>
2017-10-22 20:56:09 -0500

    Add start and end time columns to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905015108) do
+ActiveRecord::Schema.define(version: 20171023015233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170905015108) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
 end
