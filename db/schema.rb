a32b30eac643fd5a8546289f94d358aa0e256f5f
jeremy perez <glyde06@hotmail.com>
2017-10-30 21:11:35 -0500

    Configured the campaign_registrations migration and ran it

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031020009) do
+ActiveRecord::Schema.define(version: 20171031020627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaign_registrations", force: :cascade do |t|
+    t.bigint "campaign_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["campaign_id"], name: "index_campaign_registrations_on_campaign_id"
+    t.index ["user_id"], name: "index_campaign_registrations_on_user_id"
+  end
 
   create_table "campaigns", force: :cascade do |t|
     t.string "title", null: false
@@ -60,4 +69,6 @@ ActiveRecord::Schema.define(version: 20171031020009) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "campaign_registrations", "campaigns"
+  add_foreign_key "campaign_registrations", "users"
 end
