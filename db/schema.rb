064b27a0f65176f14d6d77d7667b030b8c7c4b0d
jeremy perez <glyde06@hotmail.com>
2017-10-30 21:05:38 -0500

    Configured the campaign migration and ran it

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031000248) do
+ActiveRecord::Schema.define(version: 20171031020009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
