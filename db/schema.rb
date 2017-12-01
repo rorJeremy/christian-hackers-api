23a8ed138835928df9388f768f26115e387e997e
jeremy perez <glyde06@hotmail.com>
2017-11-30 22:03:17 -0600

    Ran 'rake db:migrate' for expert migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031020627) do
+ActiveRecord::Schema.define(version: 20171201034801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20171031020627) do
     t.datetime "updated_at", null: false
     t.datetime "start_time"
     t.datetime "end_time"
+  end
+
+  create_table "experts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.string "main_link"
+    t.string "secondary_link"
+    t.integer "week"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
