d770e68f501cdf8a54f1ebce86904a10c71af4e7
jeremy perez <glyde06@hotmail.com>
2017-11-30 22:44:40 -0600

    Add styling to 'example' pages

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -69,6 +69,6 @@ class ExamplesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def example_params
-      params.fetch(:example, {})
+      params.require(:example).permit(:title, :description, :main_link, :secondary_link, :week)
     end
 end
